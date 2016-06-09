04c054b210b1ca46a5eb47270c5d2a500a2f810d
Eduardo Nunes <esnunes@gmail.com>
2016-06-09 16:02:53 -0300

    Add code boilerplate to model interface

--- core/model.go
+++ core/model.go
@@ -1,6 +1,9 @@
 package core
 
-import "errors"
+import (
+	"errors"
+	"log"
+)
 
 type Model struct {
 	cards []*Card
@@ -29,4 +32,21 @@ func (m *Model) AddCard(opts *AddCardOptions) (*Card, error) {
 
 func (m *Model) ListCards() []*Card {
 	return m.cards
+}
+
+func (m *Model) UpdateCardStatus(cid, status string) {
+	log.Printf("UpdateCardStatus(%s, %s)", cid, status)
+}
+
+func (m *Model) ClearActionLogs(cid string) {
+	log.Printf("ClearActionLog(%s)", cid)
+}
+
+func (m *Model) CreateActionLog(cid string, action Action) {
+	log.Printf("CreateActionLog(%s, %v)", cid, action)
+	// default status = Waiting
+}
+
+func (m *Model) UpdateActionLogStatus(cid string, action Action, status, msg string) {
+	log.Printf("UpdateActionLogStatus(%s, %s, %s, %s)", cid, action, status, msg)
 }
