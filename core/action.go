4dd8fa70c9e1e2c8116b39184af589a8e9d7390b
Eduardo Nunes <esnunes@gmail.com>
2016-06-09 16:03:41 -0300

    Add possible execution statuses of an action

--- core/action.go
+++ core/action.go
@@ -9,6 +9,13 @@ const (
 	ActionCreateMergeRequestToDevelop = "CreateMergeRequestToDevelop"
 )
 
+const (
+	ActionStatusWaiting    = "waiting"
+	ActionStatusProcessing = "processing"
+	ActionStatusOK         = "ok"
+	ActionStatusFailed     = "failed"
+)
+
 type Action string
 
 type ActionHandler func(config *viper.Viper, card *Card) error
